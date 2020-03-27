d96084cfb1e87aef2fb52b1c25c7c6b4516dfa74
Galvus Damor <behnkeg@informatik.uni-freiburg.de>
2020-03-27 17:56:22 +0100

    empty files for duplicate checking

---
+++ src/duplicate.cpp
@@ -0,0 +1,15 @@
+#include "duplicate.h"
+
+void unify_duplicates(const Domain & domain, const Problem & problem,
+		std::vector<Fact> & reachableFacts,
+		std::vector<GroundedTask> & reachableTasks,
+		std::vector<GroundedMethod> & reachableMethods,
+		std::vector<bool> & prunedTasks,
+		std::vector<bool> & prunedFacts,
+		std::vector<bool> & prunedMethods,
+		bool quietMode	
+		){
+
+	//exit(-1);
+}
+
---
+++ src/duplicate.h
@@ -0,0 +1,16 @@
+#ifndef DUPLICATE_H_INCLUDED
+#define DUPLICATE_H_INCLUDED 
+
+#include "model.h"
+
+void unify_duplicates(const Domain & domain, const Problem & problem,
+		std::vector<Fact> & reachableFacts,
+		std::vector<GroundedTask> & reachableTasks,
+		std::vector<GroundedMethod> & reachableMethods,
+		std::vector<bool> & prunedTasks,
+		std::vector<bool> & prunedFacts,
+		std::vector<bool> & prunedMethods,
+		bool quietMode	
+		);
+#endif
+
